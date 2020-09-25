5252a1fc509fea4e829593587253d5b85e4e81f3
7generationsAnthony <65260566+7generationsAnthony@users.noreply.github.com>
2020-09-25 14:15:30 -0500

    Adding menu controller

---
+++ agMinecraftUnity/Assets/Scripts/menuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuManager : MonoBehaviour
+{
+    public Button backyard;
+    public Button management;
+    public Button quit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backyard.onClick.AddListener(goToBackyard);
+        management.onClick.AddListener(goToManagement);
+        management.onClick.AddListener(quitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void goToBackyard()
+    {
+        SceneManager.LoadSceneAsync("Backyard");
+    }
+    private void goToManagement()
+    {
+        SceneManager.LoadSceneAsync("Management");
+    }
+    private void quitGame()
+    {
+        Application.Quit();
+    }
+}
