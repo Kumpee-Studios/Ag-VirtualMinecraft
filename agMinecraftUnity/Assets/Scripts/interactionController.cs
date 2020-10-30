bc4b17959633db7394a25ac5d177bf60c51733eb
7generationsAnthony <65260566+7generationsAnthony@users.noreply.github.com>
2020-10-30 00:57:48 -0500

    Updating to Unity 2020.1.11. Added ability to plant carrots.

--- agMinecraftUnity/Assets/Scripts/interactionController.cs
+++ agMinecraftUnity/Assets/Scripts/interactionController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class interactionController : MonoBehaviour
 {
     public playerController playercontroller;
     public GameObject interactButton;
+    private bool dirt = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button interactbuttonPart = interactButton.GetComponent<Button>();
+        interactbuttonPart.onClick.AddListener(handleInteraction);
     }
 
     // Update is called once per frame
@@ -26,11 +29,24 @@ public class interactionController : MonoBehaviour
      else if(collision.gameObject.tag == "Dirt" && playercontroller.canDoStuff == true)
         {
             interactButton.SetActive(true);
+            dirt = true;
         }   
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         playercontroller.canHoe = true; //so this isn't entirely foolproof, will need to adjust multiple bools possibly
         interactButton.SetActive(false);//not sure since I might be able to figure out what this collides with from player controller using tags
+        dirt = false;
+    }
+    public void handleInteraction()
+    {
+        if (dirt)
+        {
+            playercontroller.StartCoroutine("plantCarrot");
+            dirt = false;//should put a boolean in the dirt controller script to check if it has been planted on already
+        } else if(playercontroller.canHoe)
+        {
+            playercontroller.hoeGround();
+        }
     }
 }
