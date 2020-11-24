a0eaba2eb16620e0de16c4d257cb112e0f062e54
Gavin L Davy <davy0020@crk.umn.edu>
2020-11-23 22:53:37 -0600

    Added basic shed and market square to tilemap

---
+++ agMinecraftUnity/Assets/Scripts/NPCControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCControllerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision) 
+    {
+        Debug.Log("No one has been able to deflect the 20-meter radius of Emerald Splash!");
+    }
+}
---
+++ agMinecraftUnity/Assets/Scripts/NPCTextBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NPCTextBox : MonoBehaviour
+{
+    public GameObject textbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player presses 'E' key this frame
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // if textbox reference exists
+            if (textbox != null)
+            {
+                // toggle textbox (visible/hidden)
+                bool wasActive = textbox.gameObject.activeSelf;
+                textbox.gameObject.SetActive(!wasActive);
+            }
+        }
+    }
+}
