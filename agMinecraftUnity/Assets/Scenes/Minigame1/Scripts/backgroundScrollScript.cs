16c6e7b572d5bca68a8c594198eea60dab910c76
Gavin L Davy <davy0020@crk.umn.edu>
2021-04-06 17:58:18 -0500

    Background scroll and dummy obstacles

---
+++ agMinecraftUnity/Assets/Scenes/Minigame1/Scripts/backgroundScrollScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScrollScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private float time = 0f;
+    void Update()
+    {
+        this.transform.Translate(new Vector3(-10f,0,0)*Time.deltaTime, Space.World);
+        time += Time.deltaTime;
+        if (time > 13.6f) {
+            time = 0f;
+            this.transform.position = Vector3.zero;
+            //For good looping, take farthest x coordinate and divide by time.
+        }
+    }
+}
